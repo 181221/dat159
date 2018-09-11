ccc4de415e6a37a618db9f895ac1ded0a2a0a505
Peder Wiig Aalen <peder.wiig@gmail.com>
2018-09-11 14:32:32 +0200

    Init

---
+++ refactoring/src/no/pederyo/Customer.java
@@ -0,0 +1,63 @@
+package no.pederyo;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+    private String _result;
+    private int frequentRenterPoints;
+    private int totalAmount;
+
+    public Customer(String name) {
+        _name = name;
+    }
+
+    public String statement() {
+
+        _result = "Rental Record for " + getName() + "\n";
+
+        Enumeration rentals = _rentals.elements();
+
+        while (rentals.hasMoreElements()) {
+
+            Rental each = (Rental) rentals.nextElement();
+
+            Movie movie = each.getMovie();
+
+            double thisAmount = each.determineAmount();
+
+            addRenterPoints(each);
+
+            totalAmount += thisAmount;
+
+            _result += "\t" + movie.getTitle()+ "\t" + String.valueOf(thisAmount) + "\n";
+        }
+
+        resultfooterLine();
+
+        return _result;
+
+    }
+
+    private void addRenterPoints(Rental each) {
+        frequentRenterPoints ++;
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) frequentRenterPoints ++;
+    }
+
+    private void resultfooterLine() {
+        _result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        _result += "You earned " + String.valueOf(frequentRenterPoints) +
+                " frequent renter points";
+    }
+
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+
+    public String getName() {
+        return _name;
+    }
+}
