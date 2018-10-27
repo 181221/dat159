b60547b766f4226655670506311b809562e79d39
Peder <peder.wiig@gmail.com>
2018-10-27 17:31:13 +0200

    Implement inputs and outputs to wallet

--- Blockchain/Oblig-UTXO/src/no/pederyo/Wallet.java
+++ Blockchain/Oblig-UTXO/src/no/pederyo/Wallet.java
@@ -4,6 +4,7 @@ import java.security.KeyPair;
 import java.security.PublicKey;
 import java.util.Collection;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Wallet {
 
@@ -27,27 +28,38 @@ public class Wallet {
     }
 
     public Transaction createTransaction(long value, String address) throws Exception {
-
-        // TODO - This is a big one
-
-        
         // 1. Collect all UTXO for this wallet and calculate balance
         long amount = getBalance();
+
         // 2. Check if there are sufficient funds --- Exception?
         if(value > amount) throw new Exception("Insufficient founds!");
+
         // 3. Choose a number of UTXO to be spent --- Strategy?
-        Collection<Output> outputs = utxoMap.values();
+        Map<Input, Output> myUtxo = collectMyUtxo(); // collecting all my utxo and spending them all.
+
         // 4. Calculate change
         long change = value - amount;
+
         // 5. Create an "empty" transaction
         Transaction transaction = new Transaction(getPublicKey());
+
         // 6. Add chosen inputs
-        transaction
+        // For each UTXO that will be consumed to make this payment, the wallet creates one input pointing to the UTXO and unlocks it with an unlocking script.
+        for (Output output : myUtxo.values())
+            transaction.addInput(new Input(output.getAddress(), 0));
+
+        Output outputSpend = new Output(amount - value, getAddress());
+
+        if(amount > value)
+            transaction.addOutput(new Output(amount - value, getAddress()));
         // 7. Add 1 or 2 outputs, depending on change
+        transaction.addOutput(outputSpend);
         // 8. Sign the transaction
+        transaction.signTxUsing(keyPair.getPrivate());
         // 9. Calculate the hash for the transaction
+        transaction.calculateTxHash();
         // 10. return
-        return null;
+        return transaction;
         
         // PS! We have not updated the UTXO yet. That is normally done
         // when appending the block to the blockchain, and not here!
@@ -56,11 +68,13 @@ public class Wallet {
 
 
     public long getBalance() {
-        return calculateBalance(utxoMap.values());
+        return calculateBalance(collectMyUtxo().values());
     }
 
     private Map<Input, Output> collectMyUtxo() {
-        return utxoMap;
+        return utxoMap.entrySet().stream()
+                .filter(map -> map.getValue().getAddress().equals(id))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     private long calculateBalance(Collection<Output> outputs) {
