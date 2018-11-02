7729c072c9b644a344bd6164cbd3a0b52acb0939
Peder <peder.wiig@gmail.com>
2018-11-02 21:58:45 +0100

    implement two click combination

--- Iot/pir.ino
+++ Iot/pir.ino
@@ -3,32 +3,66 @@ int YELLOW = 12;
 int GREEN = 11;
 int BUTTON_ON = 6;
 int BUTTON_OFF = 2;
+int PIR_SENSOR = 4;
 
-int button_on_state = 0; 
-int button_off_state = 0; 
+int button_on_state = LOW;
+int button_off_state = LOW;
+
+int last_button_on_state = LOW;
+int last_button_off_state = LOW;
+
+int PIR_STATE = LOW;
+
+int val = 0;
+int STATE = 1;
+
+const int LOCKED = 0;
+const int UNLOCKED = 1;
+int WAITING = 1;
+int buttonPushCounter = 0;
+int code = 0;
+int code1 = 0;
+
 void setup()
-    {
-    pinMode(RED, OUTPUT);
-    pinMode(YELLOW, OUTPUT);
-    pinMode(GREEN, OUTPUT);
-    pinMode(BUTTON_ON, INPUT);
-    pinMode(BUTTON_OFF, INPUT);
+{
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BUTTON_ON, INPUT);
+  pinMode(BUTTON_OFF, INPUT);
+  pinMode(PIR_SENSOR, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-    button_on_state = digitalRead(BUTTON_ON);
-    button_off_state = digitalRead(BUTTON_OFF);
-    digitalWrite(13, HIGH);
-    delay(1000); // Wait for 1000 millisecond(s)
-    digitalWrite(13, LOW);
-    delay(1000); // Wait for 1000 millisecond(s)
-    
-    if (button_on_state == HIGH) {
-    // turn LED on:
-    digitalWrite(GREEN, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(GREEN, LOW);
+  switch (PIR_STATE)
+  {
+  case LOCKED:
+    digitalWrite(RED, HIGH);
+    digitalWrite(YELLOW, LOW);
+    break;
+  case UNLOCKED:
+    digitalWrite(RED, LOW);
+    digitalWrite(YELLOW, HIGH);
+    while (WAITING)
+    {
+      button_on_state = digitalRead(BUTTON_ON);
+
+      while(button_on_state != last_button_on_state){
+        if(button_on_state == HIGH){
+          button_off_state = digitalRead(BUTTON_OFF);
+          if(button_off_state != last_button_off_state){
+            if(button_off_state == HIGH){
+              digitalWrite(GREEN, HIGH)
+            }
+          }
+        }
+      }
+      last_button_on_state = button_on_state;
+      last_button_off_state = last_button_off_state;
+    }
+    break;
   }
+  PIR_STATE = digitalRead(PIR_SENSOR);
 }
