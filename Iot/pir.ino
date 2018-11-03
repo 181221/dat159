e52503cbe04d6745389dc016b643fd1cc526c879
Peder <peder.wiig@gmail.com>
2018-11-03 17:56:20 +0100

    fix an issue in unlocked state.

--- Iot/pir.ino
+++ Iot/pir.ino
@@ -1,35 +1,21 @@
-int RED = 13;
-int YELLOW = 12;
-int GREEN = 11;
-int BUTTON_ON = 6;
-int BUTTON_OFF = 2;
-int PIR_SENSOR = 4;
 
-int button_on_state;
-int button_off_state;
-int ledState = HIGH;
+const int RED = 13;
+const int YELLOW = 12;
+const int GREEN = 11;
+const int PIR_SENSOR = 4;
+const int LOCKED = 0;
+const int UNLOCKED = 2;
+const int WAITING = 1; 
+unsigned long Time = 10000;
 
-int last_button_on_state = LOW;
-int last_button_off_state = LOW;
 
 int PIR_STATE = LOW;
-
-int val = 0;
-int STATE = 1;
-
-const int LOCKED = 0;
-const int UNLOCKED = 1;
-int WAITING = 1;
-
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
-unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 
 class Button
 {
 private:
   bool _state;
   uint8_t _pin;
-  uint8_t _counter;
 
 public:
   Button(uint8_t pin) : _pin(pin) {}
@@ -48,23 +34,92 @@ public:
       _state = v;
       if (_state)
       {
-        _counter += 1;
         return true;
       }
     }
     return false;
   }
-
-  int pressed()
-  {
-    return _counter;
+  void resetState(){
+    _state = 0;
   }
 };
 
 Button myButton(6);
 Button myButton2(2);
 
+int state = 0;
+int lastState = state;
 void setup()
+{
+  pinSetup();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  PIR_STATE = digitalRead(PIR_SENSOR);
+  if(state != lastState){
+    PIR_STATE = UNLOCKED;
+  }else {
+    state = PIR_STATE;
+  }
+
+  switch (state)
+  {
+  case LOCKED:
+    digitalWrite(RED, HIGH);
+    digitalWrite(YELLOW, LOW);
+    break;
+  case UNLOCKED:
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(GREEN, HIGH);
+    delay(Time);
+    digitalWrite(GREEN, LOW);
+    myButton.resetState();
+    myButton2.resetState();
+    state = LOCKED;
+    break;
+  case WAITING:
+    digitalWrite(RED, LOW);
+
+    digitalWrite(YELLOW, HIGH);
+
+    int rightCombination = 1;
+    
+    unsigned long lastDebounceTime = millis();
+    unsigned long DebounceTime = lastDebounceTime;
+    
+
+    while (rightCombination && (DebounceTime - lastDebounceTime) < Time)
+    {
+      if (myButton.isReleased())
+      {
+        blink();
+        while (!myButton.isReleased())
+        {
+          if (myButton2.isReleased())
+          {
+            blink();
+            state = UNLOCKED;
+            break;
+          }
+        }
+        rightCombination = 0;
+      }
+      else if (myButton2.isReleased())
+      {
+        blink();
+        rightCombination = 0;
+      }
+      DebounceTime = millis();
+      delay(50);
+    }
+    break;
+  }
+}
+
+void pinSetup()
 {
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT);
@@ -72,42 +127,12 @@ void setup()
   myButton.begin();
   myButton2.begin();
   pinMode(PIR_SENSOR, INPUT);
-  Serial.begin(9600);
 }
 
-void loop()
-{
-  switch (PIR_STATE)
-  {
-  case LOCKED:
-    digitalWrite(RED, HIGH);
-    digitalWrite(YELLOW, LOW);
-    break;
-  case UNLOCKED:
-    digitalWrite(RED, LOW);
-    digitalWrite(YELLOW, HIGH);
-    int rightCombination = 1;
-    while (rightCombination)
-    {
-      if (myButton.isReleased())
-      {
-        while (!myButton.isReleased())
-        {
-          if (myButton2.isReleased())
-          {
-            digitalWrite(YELLOW, LOW);
-            digitalWrite(GREEN, HIGH);
-          }
-        }
-        rightCombination = 0;
-      }else if(myButton2.isReleased()){
-        rightCombination = 0;
-      }
-      delay(50);
-    }
 
-    
-    break;
-  }
-  PIR_STATE = digitalRead(PIR_SENSOR);
+void blink()
+{
+  digitalWrite(YELLOW, LOW);
+  delay(100);
+  digitalWrite(YELLOW, HIGH);
 }
