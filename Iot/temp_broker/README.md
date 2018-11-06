5aa254154c78feac5859e09f04278095fb70ce01
Peder <peder.wiig@gmail.com>
2018-11-06 17:46:05 +0100

    implement ardunio with nodered and mqttcloud

--- Iot/temp_broker/README.md
+++ Iot/temp_broker/README.md
@@ -1,1 +1,3 @@
-[Setup](http://www.esp32learning.com/code/esp32-and-dht11-sensor-example.php?fbclid=IwAR2SDatFyJ8uye8T7j_tuHzW4yVn-1PlBxK8YjSSO2cdSF23kK0p6eUlBYA)
+### Esp32 with node-red and mqtt cloud
+
+![alt text](./screens/flowchart.PNG)
--- Iot/temp_broker/temp_client/temp_client.ino
+++ Iot/temp_broker/temp_client/temp_client.ino
@@ -1,87 +1,113 @@
 #include "DHT.h"
-//here we use 14 of ESP32 to read data
 #define DHTPIN 23
-//our sensor is DHT11 type
 #define DHTTYPE DHT11
-//create an instance of DHT sensor
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "";
-const char* password =  "";
-const char* mqtt_server = "m23.cloudmqtt.com";
+const char *ssid = "hodden";
+const char *password = "heaton27";
+const char *mqtt_server = "m23.cloudmqtt.com";
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(115200);
-  Serial.println("DHT11 sensor!");
-  //call begin to start sensor
   dht.begin();
   WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-  
-  
 
   Serial.println("Connected to the WiFi network");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
+
   client.setServer(mqtt_server, 10113);
   client.setCallback(callback);
-  while (!client.connected()) {
+
+  while (!client.connected())
+  {
     Serial.println("Connecting to MQTT...");
- 
-    if (client.connect("ESP32Client", "", "" )) {
- 
-      Serial.println("connected");  
- 
-    } else {
- 
+
+    if (client.connect("ESP32Client", "rlmlvquc", "ck_xPRfR8TaT"))
+    {
+
+      Serial.println("connected");
+    }
+    else
+    {
+
       Serial.print("failed with state ");
       Serial.print(client.state());
-      delay(2000);
- 
+      delay(20000);
     }
-}
+  }
   client.publish("esp/test", "Hello from ESP8266");
-  client.subscribe("esp/test");
+  client.publish("lightStatus", "OFF");
+  client.subscribe("output");
+  client.subscribe("test");
 }
 
-
-
-void callback(char* topic, byte* message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
   String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
+
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
   }
   Serial.println();
+
+  if (String(topic) == "output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(LED_BUILTIN, HIGH);
+      client.publish("lightStatus", "ON");
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(LED_BUILTIN, LOW);
+      client.publish("lightStatus", "OFF");
+    }
+  }
+  
+
 }
 
-void reconnect() {
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client"))
+    {
       Serial.println("connected");
       // Subscribe
       client.subscribe("esp32/output");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -91,19 +117,42 @@ void reconnect() {
   }
 }
 
-
-void loop() {
-
-  //use the functions which are supplied by library.
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
   }
+  client.loop();
+
+  long now = millis();
+
+  if(now - lastMsg > 5000) {
+    //use the functions which are supplied by library.
+    lastMsg = now;
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t))
+    {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    print(h, t);
+    char tempString[8];
+    char humidString[8];
+    dtostrf(t, 1, 2, tempString);
+    dtostrf(h, 1, 2, humidString);
   
+    client.publish("temperature", tempString);
+    client.publish("humidity", humidString);
+  }
+
+}
+
+void print(float h, float t)
+{
+
   // print the result to Terminal
   Serial.print("Humidity: ");
   Serial.print(h);
@@ -112,9 +161,4 @@ void loop() {
   Serial.print(t);
   Serial.println(" *C ");
   //we delay a little bit for next read
-  
-  char tempString[8];
-  dtostrf(t, 1, 2, tempString);
-  client.publish("Temperature",tempString);
-  delay(2000);
 }
